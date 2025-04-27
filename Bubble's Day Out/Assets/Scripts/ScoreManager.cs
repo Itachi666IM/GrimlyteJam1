8b458b4635d3e95d979d617e134f0ceb3d385909
Sanj1t <79507387+Itachi666IM@users.noreply.github.com>
2025-04-27 22:09:11 +0530

    Game complete minus pp and sfx

---
+++ Bubble's Day Out/Assets/Scripts/ScoreManager.cs
@@ -0,0 +1,69 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+using UnityEngine.SceneManagement;
+
+public class ScoreManager : MonoBehaviour
+{
+    public TMP_Text scoreText;
+    int score;
+    public float minTimeToNextPoint;
+    public float maxTimeToNextPoint;
+    float timeToNextPoint;
+    float nextPointTime;
+
+    [HideInInspector]public bool isPlayerDead = false;
+
+    public GameObject bubbleEffect;
+    public GameObject winPanel;
+    public GameObject losePanel;
+    // Start is called before the first frame update
+    void Start()
+    {
+        timeToNextPoint = maxTimeToNextPoint;
+        score = 0;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(!isPlayerDead)
+        {
+            if (Time.time > nextPointTime)
+            {
+                score++;
+                nextPointTime = Time.time + timeToNextPoint;
+                timeToNextPoint -= (minTimeToNextPoint / maxTimeToNextPoint);
+                if (timeToNextPoint < minTimeToNextPoint)
+                {
+                    timeToNextPoint = minTimeToNextPoint;
+                }
+            }
+            scoreText.text = score.ToString();
+        }
+        else
+        {
+            bubbleEffect.SetActive(false);
+            if(score < 50)
+            {
+                losePanel.SetActive(true);
+            }
+            else
+            {
+                winPanel.SetActive(true);
+            }
+        }
+
+    }
+
+    public void RestarttGame()
+    {
+        SceneManager.LoadScene("Game");
+    }
+
+    public void QuitGame()
+    {
+        Application.Quit();
+    }
+}
--- Bubble's Day Out/Assets/Scripts/Spike.cs
+++ Bubble's Day Out/Assets/Scripts/Spike.cs
@@ -5,11 +5,18 @@ using UnityEngine.SceneManagement;
 
 public class Spike : MonoBehaviour
 {
+    ScoreManager scoreManager;
+
+    private void Start()
+    {
+        scoreManager = FindObjectOfType<ScoreManager>();
+    }
     private void OnTriggerEnter2D(Collider2D collision)
     {
         if(collision.tag == "Player")
         {
-            SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+            Destroy(collision.gameObject);
+            scoreManager.isPlayerDead = true;
         }
     }
 }
